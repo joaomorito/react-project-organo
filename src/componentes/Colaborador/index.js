68c52d7395397d7b2495670fccc2e6a7d0e160d1
joaomorito <joaomorito04@gmail.com>
2025-01-08 13:28:28 -0300

    Card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/joaomorito.png' alt='João Victor'/>
+            </div>
+            <div className='rodape'>
+                <h4>João</h4>
+                <h5>Desenvolvedor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
